028358f23a43697415913b09fb6fb90b2acb4e3e
Gabriel Zanon <gabrielzanon77@outlook.com>
2021-06-18 01:59:43 -0300

    Create GET, POST, PUT and DELETE methods

--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -1,7 +1,63 @@
 import { Request, Response } from 'express';
+import IClient from '../interfaces/IClient';
+import Client from '../models/Client'
 
-const getAllClients = (req: Request, res: Response) => {
+async function getAllClients (req: Request, res: Response) {
+  const client = await Client.find();
 
+  if (client.length === 0) {
+    return res.status(400).send({ error: 'No clients to show!' });
+  }
+
+  return res.send({ client });
 }
 
-export default { getAllClients }
+async function createClient (req: Request, res: Response) {
+  const { cpf } = req.body;
+
+  try {
+    if (await Client.findOne({ cpf })) {
+      return res.status(400).send({ error: 'Client already exists!' });
+    }
+
+    await Client.create(req.body);
+
+    return res.status(200).send({ message: 'Client created!' });
+  } catch (err) {
+    return res.status(400).send({ error: 'Registration failed' });
+  }
+}
+
+async function updateClientById (req: Request, res: Response) {
+  const update = {} as IClient;
+  const { _id, name, cpf } = req.body;
+
+  if (name && name != '') update.name = name;
+  if (cpf && cpf != '') update.cpf = cpf;
+
+  try {
+    const client = await Client.updateOne({ _id }, update);
+    if (client.nModified === 0) {
+      return res.status(400).send({ error: 'Zero updates available' });
+    } else {
+      return res.status(200).send({ message: 'Client updated!' });
+    }
+  } catch (err) {
+    return res.status(400).send({ error: 'This client does not exist!' });
+  }
+}
+
+async function deleteClientById (req: Request, res: Response) {
+  const { _id } = req.body;
+
+  try {
+    await Client.deleteOne({ _id });
+
+
+    return res.status(200).send({ message: 'Client deleted!' });
+  } catch (err) {
+    return res.status(400).send({ error: 'This client does not exist!' });
+  }
+}
+
+export default { getAllClients, createClient, updateClientById, deleteClientById };
--- src/routes/clientRoutes.ts
+++ src/routes/clientRoutes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import connectToMongo from '../connection';
 import clientController from '../controllers/clientController';
 
 const router = Router();
+connectToMongo();
 
 router.get('/clients', clientController.getAllClients);
+router.post('/addClient', clientController.createClient);
+router.put('/clients/:client_id', clientController.updateClientById);
+router.delete('/clients/:client_id', clientController.deleteClientById);
 
 export default router;
