863a37480c9562fcea921dd3512126a10e9c91d8
Gabriel Zanon <gabrielzanon77@outlook.com>
2021-06-18 00:19:32 -0300

    Create Client Routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import config from './config/config';
+import clientRouter from './routes/clientRoutes';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use('/api', clientRouter);
 
 app.listen(config.server.port, () => {
   console.log(`Server running on ${config.server.hostname}:${config.server.port}`);
